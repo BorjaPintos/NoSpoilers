8118c01ea401c283a6151797cf0eb090d5d1c8d0
waky <waky@developer>
2016-05-16 22:38:55 +0200

    add option fot show spoiler onClick

--- NoSpoilersLaVozDeGalicia.user.js
+++ NoSpoilersLaVozDeGalicia.user.js
@@ -1,21 +1,35 @@
 // ==UserScript==
 // @name         NoSpoilersLaVozDeGalicia
-// @version      0.1
-// @description  try to take over the world!
+// @version      0.2
+// @description  Quita spoilers de la voz de galicia
 // @author       Borja Pintos
 //@downloadURL   https://github.com/BorjaPintos/NoSpoilers/blob/master/NoSpoilersLaVozDeGalicia.user
 // @match        *://*.lavozdegalicia.es/*
 // @grant        none
 // ==/UserScript==
 
-
+var spoilers;
 
 function init() {
-	var spoilers = document.getElementsByClassName("television");
+	spoilers = document.getElementsByClassName("television");
+    
     var length = spoilers.length;
     for (var i = 0; i<length;i++){
-        spoilers[i].remove();
+        spoilers[i].style.visibility = "hidden";
+        var p = document.createElement("h1");
+        var text = document.createTextNode("Posible Spoiler, click 'aquí' para ver");
+        p.appendChild(text);
+        p.id = i;
+        p.addEventListener('click', function(){
+           restaurar(this, this.id);
+        });
+        spoilers[i].parentNode.insertBefore(p, spoilers[i]);
     }
+}
+
+function restaurar(h1, nSpoiler){
+    h1.remove();
+    spoilers[nSpoiler].style.visibility = "visible";
 }
 
 init();
